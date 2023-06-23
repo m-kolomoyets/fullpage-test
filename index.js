6637b651bd395dcf6c2fe9df4fa227599bf74f1a
m.kolomoyets <mykola.kolomoyets@phenomenon-studio.com>
2023-06-23 15:05:52 +0300

    feat(gameplay): added gameplay slider

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-$(document).ready(function () {
-  $('#fullpage').fullpage({
-    autoScrolling: true,
-    scrollHorizontally: true,
-    normalScrollElements: '.swiper, .info',
-    scrollingSpeed: 1700
-  });
 
+const registerGSAPPlugins = () => {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+}
+
+const initFullPageScroll = () => {
+  $('#fullpage').fullpage({
+    // autoScrolling: true,
+    scrollHorizontally: true,
+    normalScrollElements: '.swiper, .info, .section[data-anchor="cards"]',
+    scrollingSpeed: 1700,
+    verticalCentered: false,
+    scrollBar: false,
+    scrollOverflow: true,
+    bigSectionsDestination: 'top',
+  });
+}
+
+const initSwiperSliders = () => {
   const swiper = new Swiper(".mySwiper", {
     spaceBetween: 10,
     slidesPerView: 4,
@@ -14,12 +25,50 @@ $(document).ready(function () {
     preventInteractionOnTransition: true,
     slideToClickedSlide: true,
   });
-
-  const swiper2 = new Swiper(".mySwiper2", {
+  const _swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     thumbs: {
       swiper: swiper,
     },
   });
+}
 
+const initCardsScrollAnimation = () => {
+  $('.fp-overflow').on('scroll', event => {
+    const scroll = event.target.scrollTop;
+
+    const clientHeight = window.innerHeight;
+
+    const activeSection = Math.ceil(scroll / clientHeight) + 1;
+
+    console.log(activeSection);
+
+    if (activeSection > 1) {
+      $(`.card:nth-child(${activeSection})`).addClass('active');
+    }
+
+    if (activeSection >= 1) {
+      $(`.card:nth-child(${activeSection})`).removeClass('inactive');
+      $(`.card:nth-child(${activeSection + 1})`).removeClass('active');
+    }
+
+    const previousSections = $(`.card:nth-child(-n+${activeSection - 1})`);
+
+    previousSections.each((index, element) => {
+      $(element).addClass('inactive');
+    });
+
+  })
+
+}
+
+$(document).ready(function () {
+  console.clear();
+
+  initFullPageScroll();
+
+  initSwiperSliders();
+
+  registerGSAPPlugins();
+  initCardsScrollAnimation();
 });
